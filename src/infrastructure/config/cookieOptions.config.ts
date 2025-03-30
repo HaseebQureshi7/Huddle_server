94c87e3227c72ad0c4d4a794a0fcfe85ade8942b
HaseebQureshi7 <qureshihaxeeb2@gmail.com>
2025-03-31 05:16:20 +0530

    chore: cookie options changed

--- src/infrastructure/config/cookieOptions.config.ts
+++ src/infrastructure/config/cookieOptions.config.ts
@@ -1,11 +1,11 @@
 import { CookieOptions } from "express";
 
-const isProduction = process.env.NODE_ENV === "production";
+const isDevelopment = process.env.NODE_ENV === "development";
 
 export const cookieOptions: CookieOptions = {
   httpOnly: true,
-  secure: isProduction, // Secure in production
-  sameSite: isProduction ? "none" : "lax", // Cross-site support only in production
+  secure: !isDevelopment, // Secure in production
+  sameSite: !isDevelopment ? "none" : "lax", // Cross-site support only in production
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   path: "/",
 };
